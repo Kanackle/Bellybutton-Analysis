9696a8fe8823040e22a10adb69dba1d44a27b068
Kanackle <Kanak.Somani02@myhunter.cuny.edu>
2022-12-03 22:24:08 -0500

    Added working code for bar graph

--- charts.js
+++ charts.js
@@ -58,31 +58,45 @@ function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
-    samplesArray = data.samplesArray;
+    var samplesArray = data.samples;
+    console.log(data);
+    console.log(samplesArray);
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-     
-    //  5. Create a variable that holds the first sample in the array.
     var resultArray = samplesArray.filter(sampleObj => sampleObj.id == sample);
+    //  5. Create a variable that holds the first sample in the array.
     var result = resultArray[0];
-    console.log(result);
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-
-
+    
+    var idArray = result.otu_ids.slice(0,10).reverse();
+    var ridArray = [];
+    var labelsArray = result.otu_labels.slice(0,10).reverse();
+    var valuesArray = result.sample_values.slice(0,10).reverse();
+    for(var i = 0; i < 10; i ++){
+        ridArray.push("OTU: " + idArray[i]);
+    }
+    console.log(valuesArray)
+    //console.log(labelsArray)
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = 
-
+    //var yticks = idArray.slice(0,10);
+    //console.log(yticks);
     // 8. Create the trace for the bar chart. 
-    var barData = [
-      
-    ];
+    var barData = [{
+      x: valuesArray,
+      y: ridArray,
+      text: labelsArray,
+      type: "bar",
+      orientation: 'h'
+    }];
     // 9. Create the layout for the bar chart. 
     var barLayout = {
-     
+      title: "Top 10 Bacteria Samples",
+      xaxis: {title: "Sample Values"},
+      yaxis: {title: "OTU ids"}
     };
     // 10. Use Plotly to plot the data with the layout. 
-    
+    Plotly.newPlot("bar", barData, barLayout)
   });
 }
